264de04e1f74679fabc7c67264c16e408f85b585
kamilz12 <kamilziolkowski231@gmail.com>
2024-07-16 14:13:17 +0200

    0.0.0

---
+++ src/test/java/com/kamilz12/vehiclemanagementsystem/VehicleManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamilz12.vehiclemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
